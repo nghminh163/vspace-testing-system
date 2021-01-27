4a122602430b7ce1f8248a19322ac599fe289ff6
Nguyễn Hoàng Minh <nghminh163@outlook.com>
2021-01-27 23:15:36 +0700

    Replace new project firebase

--- src/configs/firebase.js
+++ src/configs/firebase.js
@@ -1,11 +1,11 @@
 export default function firebaseConfig() {
   return {
-    apiKey: "AIzaSyAxoho53Hgdhweiunxjen2vMhWmOUidx0c",
-    authDomain: "vspace-vn.firebaseapp.com",
-    projectId: "vspace-vn",
-    storageBucket: "vspace-vn.appspot.com",
-    messagingSenderId: "356456684546",
-    appId: "1:356456684546:web:9eff2df8ddb17e302d5b1b",
-    measurementId: "G-SZDZYGK7JY"
+    apiKey: "AIzaSyCP89_sCZaA58fwD3gy4wrvA9l-OQ6PPrE",
+    authDomain: "vspacevn-testing-system.firebaseapp.com",
+    projectId: "vspacevn-testing-system",
+    storageBucket: "vspacevn-testing-system.appspot.com",
+    messagingSenderId: "1068950312495",
+    appId: "1:1068950312495:web:d5ac83deca9a93bda618da",
+    measurementId: "G-CXTL5S425N"
   };
 }
